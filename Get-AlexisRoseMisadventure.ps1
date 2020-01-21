61ddbdf881ecfaf111171e3c7f1fb7638fdbcc2c
David Steimle <davidsteimle@gmail.com>
2020-01-21 17:12:40 +0000

    initial commit

---
+++ Get-AlexisRoseMisadventure.ps1
@@ -0,0 +1,32 @@
+#!/usr/bin/env pwsh
+
+function Get-MisadventureText{
+    $Choice1 = $One | Get-Random
+    $Choice2 = $Two | Get-Random
+    $Choice3 = $Three | Get-Random
+    $Choice4 = $Four | Get-Random
+    $Choice5 = $Five | Get-Random
+    "Oh really, David? Okay, well you try being $Choice1 and having to $Choice2 your way out of $Choice3's $Choice4 with only $Choice5, David. Ugh!"
+}
+
+$One = @(
+
+)
+
+$Two = @(
+
+)
+
+$Three = @(
+
+)
+
+$Four = @(
+
+)
+
+$Five = @(
+
+)
+
+
